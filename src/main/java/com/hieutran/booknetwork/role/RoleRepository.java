f0f549b040804069828c0f8e82806657b2ed83e6
hieutran1220 <156657159+hieutran1220@users.noreply.github.com>
2024-04-17 17:20:17 +0700

    init project

---
+++ src/main/java/com/hieutran/booknetwork/role/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.hieutran.booknetwork.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String role);
+
+}
